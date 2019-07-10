3ed68d494dd8d6df8f93c51395263c5f48b86f84
Loy van Beek <loy.vanbeek@gmail.com>
2019-07-10 15:07:22 +1000

    initial version of HMI Telegram

---
+++ src/hmi_telegram/telegram_autocompletions.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import random
+
+import rospy
+from hmi import AbstractHMIServer, HMIResult
+from hmi.common import random_sentence, result_from_ros, parse_sentence
+from grammar_parser.cfgparser import CFGParser
+from threading import Event
+
+from std_msgs.msg import String
+from telegram_ros.msg import Options
+
+wait_time = 5
+
+
+class TelegramAutocompletions(AbstractHMIServer):
+    def __init__(self, *args, **kwargs):
+        super(TelegramAutocompletions, self).__init__(*args, **kwargs)
+
+        self._messsage_sub = rospy.Subscriber('message_to_ros', String, self._message_cb)
+        self._options_pub = rospy.Publisher('options_from_ros', Options, queue_size=1)
+
+        self._grammar_parser = None  # type: CFGParser
+        self._target = ''
+        self._intermediate_answer = []
+        self._answer_ready = Event()
+
+    @property
+    def intermediate_sentence(self):
+        return ' '.join(self._intermediate_answer)
+
+    def _determine_answer(self, description, grammar, target, is_preempt_requested):
+        if is_preempt_requested and self._target:
+            rospy.logwarn("Preempt requested while a query was being answered")
+            self._answer_ready.set()
+        else:
+            rospy.logwarn("Preempt requested while NO query was being answered")
+
+        rospy.loginfo('Sending to telegram')
+        self._intermediate_answer = []
+        self._target = target
+        self._grammar_parser = CFGParser.fromstring(grammar)
+        self._grammar_parser.verify()
+
+        self._options_pub.publish(Options(question=description,
+                                          options=sorted(set(
+                                              self._grammar_parser.next_word(self._target,self._intermediate_answer)))))
+
+        if self._answer_ready.wait():
+            self._answer_ready.clear()
+
+            try:
+                sentence = ' '.join(self._intermediate_answer)
+                rospy.loginfo("Gathered sentence: %s", sentence)
+                if sentence:
+                    semantics = parse_sentence(sentence, grammar, target)
+                    rospy.loginfo("Parsed semantics: %s", semantics)
+
+                    self._intermediate_answer = []
+                    self._target = ''
+                    return HMIResult(sentence, semantics)
+            except Exception:
+                raise
+        else:
+            rospy.loginfo("Telegram took to look to complete the query")
+
+    def _message_cb(self, msg):
+        # type: (String) -> None
+        rospy.loginfo("Got another bit: {}".format(msg.data))
+
+        if msg.data == '.' or msg.data.endswith('.'):
+            rospy.loginfo("Done: '{}' signifies end of sentence")
+            self._answer_ready.set()
+        else:
+            self._intermediate_answer += [msg.data]
+            rospy.loginfo("Intermediate sentence is '{}'".format(self.intermediate_sentence))
+
+            # Below, the sentence is split right after we join it.
+            # This allows the user to manuallly type (not pick from button) several words at once
+            completions = sorted(set(
+                self._grammar_parser.next_word(self._target, self.intermediate_sentence.split(' '))))
+
+            if completions:
+                rospy.loginfo("Done: there are {} completions available".format(len(completions)))
+                self._options_pub.publish(Options(options=completions))
+            else:
+                rospy.loginfo("Done: there are no more completions available after '{}'".format(self.intermediate_sentence))
+                self._answer_ready.set()
