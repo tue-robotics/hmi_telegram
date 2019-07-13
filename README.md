3e6020dc524bcae122e8f31c5f3533168804b4c6
Loy van Beek <loy.vanbeek@gmail.com>
2019-07-13 14:38:28 +0200

    Add image and launch file with basic test

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hmi_telegram
 Server for [HMI](https://github.com/tue-robotics/hmi) to iteratively suggestion options to be presented as a custom keyboard for Telegram, through [telegram_ros](https://github.com/tue-robotics/telegram_ros)
+
+![Screenshot that builds the sentence 'I would like a ...'](https://github.com/tue-robotics/hmi_telegram/raw/master/screenshot.jpg "I would like a...")
+
+# Running
+There is an example launch file:
+```bash
+roslaunch hmi_telegram test_autocompletions.launch token:=5693...
+```
+Tokens for telegram can be obtained via BotFather on Telegram
---
+++ test/test_autocompletions.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import actionlib
+from hmi_msgs.msg import QueryAction
+from hmi import Client
+
+order_grammar = """
+O[P] -> ORDER[P] | can i have a ORDER[P] | i would like ORDER[P] | can i get ORDER[P] | could i have ORDER[P] | may i get ORDER[P] | bring me ORDER[P]
+ORDER[<A1>] -> ITEMS[A1] | DET ITEMS[A1]
+ORDER[<A1, A2>] -> ITEMS[A1] ITEMS[A2] | DET ITEMS[A1] DET ITEMS[A2] | DET ITEMS[A1] and DET ITEMS[A2] | ITEMS[A1] and ITEMS[A2]
+ORDER[<A1, A2, A3>] -> ITEMS[A1] ITEMS[A2] ITEMS[A3] | ITEMS[A1] ITEMS[A2] and ITEMS[A3] |DET ITEMS[A1] DET ITEMS[A2] DET ITEMS[A3] | DET ITEMS[A1] and DET ITEMS[A2] and DET ITEMS[A3] | DET ITEMS[A1] DET ITEMS[A2] and DET ITEMS[A3]
+DET -> a | an
+"""
+
+candies = ["biscuit", "frosty_fruits", "snakes"]
+drinks = ["beer", "chocolate_milk", "coke", "juice", "lemonade", "water"]
+food = ["carrot", "cereals", "noodles", "onion", "vegemite"]
+fruits = ["apple", "kiwi", "lemon", "orange", "pear"]
+snacks = ["cheetos", "doritos", "shapes_chicken", "shapes_pizza", "twisties"]
+
+
+for item in candies + drinks + food + fruits + snacks:
+    order_grammar += "\nITEMS['{}'] -> {}[B]".format(item, item.replace('_', ' '))
+
+if __name__ == "__main__":
+    import rospy
+    rospy.init_node('test_autocompletions')
+
+    sac = actionlib.SimpleActionClient('/hmi', QueryAction)
+    sac.wait_for_server()
+    client = Client(simple_action_client=sac)
+
+    rospy.sleep(rospy.Duration(5))
+
+    raw_input("Press enter to continue after you have started a conversation with the bot")
+
+    result = client.query("What is your order?",
+                          order_grammar, "O",
+                          timeout=300)
+
+    print(result)
