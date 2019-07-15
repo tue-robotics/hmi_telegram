97bece09fc310ce4792a254842c55e4c7ecfa3fa
Loy van Beek <loy.vanbeek@gmail.com>
2019-07-15 20:09:38 +0200

    Extend test_autocompletions.py to load arbitrary grammars from file

--- test/test_autocompletions.py
+++ test/test_autocompletions.py
@@ -3,8 +3,9 @@
 import actionlib
 from hmi_msgs.msg import QueryAction
 from hmi import Client
+import os
 
-order_grammar = """
+grammar = """
 O[P] -> ORDER[P] | can i have a ORDER[P] | i would like ORDER[P] | can i get ORDER[P] | could i have ORDER[P] | may i get ORDER[P] | bring me ORDER[P]
 ORDER[<A1>] -> ITEMS[A1] | DET ITEMS[A1]
 ORDER[<A1, A2>] -> ITEMS[A1] ITEMS[A2] | DET ITEMS[A1] DET ITEMS[A2] | DET ITEMS[A1] and DET ITEMS[A2] | ITEMS[A1] and ITEMS[A2]
@@ -20,11 +21,27 @@ snacks = ["cheetos", "doritos", "shapes_chicken", "shapes_pizza", "twisties"]
 
 
 for item in candies + drinks + food + fruits + snacks:
-    order_grammar += "\nITEMS['{}'] -> {}[B]".format(item, item.replace('_', ' '))
+    grammar += "\nITEMS['{}'] -> {}[B]".format(item, item.replace('_', ' '))
+
+target = 'O'
+
+def read_valid_file(p, arg):
+    if not os.path.exists(arg):
+        p.error("The file %s does not exist!" % arg)
+    else:
+        return open(arg, 'r').read()
 
 if __name__ == "__main__":
     import rospy
     rospy.init_node('test_autocompletions')
+
+    target = rospy.get_param('~target', target)
+    grammar_file = rospy.get_param('~grammar_file', None)
+
+    # Verify the specified grammar
+    if grammar_file and target:
+        with open(grammar_file) as gf:
+            grammar = gf.read()
 
     sac = actionlib.SimpleActionClient('/hmi', QueryAction)
     sac.wait_for_server()
@@ -36,7 +53,7 @@ if __name__ == "__main__":
     raw_input("Press enter to continue after you have started a conversation with the bot")
 
     result = client.query("What is your order?",
-                          order_grammar, "O",
+                          grammar, target,
                           timeout=300)
 
     print(result)
