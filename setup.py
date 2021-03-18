129f3d4bd27b0f6b6b972689abba57229f7e923b
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-03-18 10:19:38 +0100

    prepare for python3/noetic

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
-setup(
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    # #  don't do this unless you want a globally visible script
+    # scripts=['bin/myscript'],
     version='0.0.1',
     packages=['hmi_telegram'],
     package_dir={'': 'src'}
 )
+
+setup(**d)
